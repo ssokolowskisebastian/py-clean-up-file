e9322b2b2c350198115b8182402e52b3b48f4276
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-30 13:23:42 +0200

    annotation type

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 import os
+from typing import Optional, Type
+import types
 
 
 class CleanUpFile:
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
     def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[types.TracebackType]
+                 ) -> None:
         os.remove(self.filename)
-
